378e4a8e6e261a5d5ab01e713e98a02573eb03e1
Darsy <Darsy@LAPTOP-O7C0HT9P>
2023-05-13 17:42:56 -0500

    finished program

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,9 +1,8 @@
 using System;
 
-// A code template for the category of things known as 
 public class Entry
 {
-    // The C# convention is to start member variables with an underscore _
+    
     public string _entryNumber = "";
     public string _dateTime = "";
     public string _promptGenerator = "";
@@ -11,13 +10,12 @@ public class Entry
     public string _journalFile = "";
 
 
-    // A special method, called a constructor that is invoked using the  
-    // new keyword followed by the class name and parentheses.
+   
     public Entry()
     {
     }
 
-    // A method that displays 
+   
     public void Display()
     {
         
